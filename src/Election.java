529e3dcadbb00c56dce62aa8da44a70cbe26a5b9
Aidenjewett905 <75604387+Aidenjewett905@users.noreply.github.com>
2025-04-18 21:30:22 -0700

    Election Class Variables
    Added the arraylists and lists that will be used by the election class.

--- src/Election.java
+++ src/Election.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
 
 public class Election {
-
+	
+	private ArrayList<Ballot> ballotList = new ArrayList<>(); //List of all cast ballots
+	private ArrayList<Candidate> candidateList = new ArrayList<>(); //List of all candidates
+	private Candidate[] electionWinners; //Array that contains the winning candidates
+	
+	public Election() {
+		
+	}
+	
 }
