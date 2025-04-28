b0750f32f0ae0bf81f69af3b331a32d34f4f6969
Aidenjewett905 <75604387+Aidenjewett905@users.noreply.github.com>
2025-04-27 22:10:19 -0700

    Election getters, setters, constructor
    Getter and setters for the Election class. Constructor was modified to take lists of candidates and ballots and copy them into the Election object.

--- src/Election.java
+++ src/Election.java
@@ -6,12 +6,34 @@ import java.io.PrintWriter;
 
 public class Election {
 	
-	private ArrayList<Ballot> ballotList = new ArrayList<>(); //List of all cast ballots
-	private ArrayList<Candidate> candidateList = new ArrayList<>(); //List of all candidates
+	private ArrayList<Ballot> ballotList; //List of all cast ballots
+	private ArrayList<Candidate> candidateList; //List of all candidates
 	private Candidate[] electionWinners; //Array that contains the winning candidates
 	
-	public Election() {
-		
+	public Election(ArrayList<Ballot> ballotList, ArrayList<Candidate> candidateList, int numOfWinners) {
+		this.ballotList = new ArrayList<>(ballotList);
+		this.candidateList = new ArrayList<>(candidateList);
+		electionWinners = new Candidate[numOfWinners];
+	}
+	
+	public void setBallot(int index, Ballot ballot) {
+		ballotList.set(index, ballot);
+	}
+	public void setCandidate(int index, Candidate candidate) {
+		candidateList.set(index, candidate);
+	}
+	public void setWinner(int index, Candidate candidate) {
+		electionWinners[index] = candidate;
+	}
+	
+	public Ballot getBallot(int index) {
+		return ballotList.get(index);
+	}
+	public Candidate getCandidate(int index) {
+		return candidateList.get(index);
+	}
+	public Candidate getWinner(int index) {
+		return electionWinners[index];
 	}
 	
 }
