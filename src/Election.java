f0cc8ec5739781285febfd5ba7365495a84dc958
Aidenjewett905 <75604387+Aidenjewett905@users.noreply.github.com>
2025-05-05 23:15:42 -0700

    removeLastPlaceCandidates method
    Added the removeLastPlaceCandidates method. This method will eliminate all candidates tied for last place providing that they can all lose without leaving a seat vacant. Otherwise, if all candidates can win (with or without vacant seats), then the method will add them to the winner list. If neither of these are possible, the method marks remaining seats as "Tied" and returns false instead of true.

--- src/Election.java
+++ src/Election.java
@@ -77,8 +77,59 @@ public class Election {
 		return false; //No seats were won
 	}
 	
-	//public boolean removeLastPlaceCandidates() {
-	//	
-	//}
+	public boolean removeLastPlaceCandidates(int numTotalSeats) { //Returns false if there is an unbreakable tie
+		int numTied = 1; //If the candidate is tied with the next one, then that means there are 2 tied candidates overall, so by default there is 1 tied candidate.
+		boolean noMoreTied = false;
+		int availableSeats = numTotalSeats - getNumWinners();
+		
+		int index = 0;
+		while(!noMoreTied)
+		{
+			if(candidateList.get(index).compareTo(candidateList.get(++index)) == 0) //This increments the index too
+			{
+				numTied++; //There is another tied candidate
+			}
+			else
+				noMoreTied = true;
+		}
+		
+		int numOfLosers = candidateList.size() - availableSeats; //Number of total candidates that have not won minus available seats
+		if(numTied <= numOfLosers) //If all remaining candidates can lose, eliminate them
+		{
+			while(numTied > 0)
+			{
+				candidateList.remove(0).eliminate();
+				numTied--;
+			}
+			
+			Collections.sort(candidateList); //Sort the candidates now that votes have been redistributed from eliminated candidates
+			
+			return true; //Candidates were eliminated
+		}
+		else if(numOfLosers <= 0) //If no other candidates must lose, then all can win
+		{
+			while(numTied > 0)
+			{
+				electionWinners.add(candidateList.remove(0));
+				numTied--;
+			}
+			while(getNumWinners() < numTotalSeats)
+			{
+				electionWinners.add(new Candidate("Vacant")); //All remaining seats cannot be filled by a candidate
+			}
+			
+			return true; //All candidates won
+		}
+		else //If the tied candidates can't all lose or win, then there is a tie that must be broken with a runoff or other process
+		{
+			while(getNumWinners() < numTotalSeats) //For all remaining seats
+			{
+				electionWinners.add(new Candidate("Tied")); //All remaining seats are tied
+			}
+			
+			return false; //Unbreakable tie, no candidates removed
+		}
+		
+	}
 	
 }
