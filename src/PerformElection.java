5b4184dbf1935bdfc63e9604e2abeda057be56e7
Aiden <Aidenjewett905@gmail.com>
2025-05-25 18:39:32 -0700

    Import data from files
    Added the required code to take the paths and values from the settings file, and the candidates from the candidates and ballots file.

--- src/PerformElection.java
+++ src/PerformElection.java
@@ -1,9 +1,102 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.File;
 
 public class PerformElection {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Scanner keyboard = new Scanner(System.in);
+		File settingsFile = new File("settings.txt"); //settings.txt is the default file path
+		Scanner settingsInput;
 		
+		ArrayList<Candidate> candidateList = new ArrayList<Candidate>();
+		ArrayList<Ballot> ballotList = new ArrayList<Ballot>();
 
+		while(!settingsFile.exists())
+		{
+			System.out.println("Default settings file not found");
+			System.out.print("Enter the path for the settings file: ");
+			settingsFile = new File(keyboard.nextLine());
+		}
+		settingsInput = new Scanner(settingsFile);
+		
+		//Get input from settings file via the default layout, any exceptions to be handled by default handler
+		//settingsInput.next() twice will clear the "setting_name =" part of the line
+		settingsInput.next();
+		settingsInput.next();
+		int numOfSeats = settingsInput.nextInt();
+		settingsInput.nextLine();
+		
+		settingsInput.next();
+		settingsInput.next();
+		Scanner candidatesInput = new Scanner(new File(settingsInput.nextLine()));
+		
+		settingsInput.next();
+		settingsInput.next();
+		Scanner ballotsInput = new Scanner(new File(settingsInput.nextLine()));
+		
+		settingsInput.next();
+		settingsInput.next();
+		PrintWriter winnersOutput = new PrintWriter(new File(settingsInput.nextLine()));
+		
+		//Get list of candidates
+		candidatesInput.nextLine(); //Clear instruction line
+		String[] candidateNameArray = candidatesInput.nextLine().split(", ");
+		
+		for(String candidateName : candidateNameArray)
+		{
+			candidateList.add(new Candidate(candidateName));
+		}
+		
+		candidatesInput.close();
+		
+		if(candidateList.size() <= numOfSeats)
+		{
+			Queue<Candidate> winners = new LinkedList<Candidate>();
+			while(candidateList.size() > 0)
+			{
+				winners.add(candidateList.remove(0));
+			}
+			while(winners.size() < numOfSeats)
+			{
+				winners.add(new Candidate("Vacant")); //All remaining seats cannot be filled by a candidate
+			}
+			
+			Election.outputWinnersStatic(winnersOutput, winners);
+			System.exit(0); //Election done
+		}
+		
+		//Get ballots and set candidates
+		ballotsInput.nextLine(); //Clear instructions
+		while(ballotsInput.hasNext())
+		{
+			String[] ballotArray = ballotsInput.nextLine().split(", ");
+			Candidate[] candidateArray = new Candidate[ballotArray.length];
+			int candidateArrayIndex = 0;
+			
+			boolean candidateAdded = false; //Used to catch an invalid candidate on the ballot
+			for(String candidateName : ballotArray)
+			{
+				for(int i = 0; i < candidateList.size(); i++)
+				{
+					if(candidateName.equals(candidateList.get(i).getName()) && !candidateAdded) //!candidateAdded is used to prevent out of bounds due to duplicate candidates
+					{
+						candidateArray[candidateArrayIndex] = candidateList.get(i);
+						candidateArrayIndex++;
+						candidateAdded = true;
+					}
+				}
+				if(!candidateAdded)
+					System.out.printf("Candidate %s does not exist, they were not added to the ballot", candidateName);
+			}
+			
+			ballotList.add(new Ballot(candidateArray));
+		}
+		
 	}
 
 }
